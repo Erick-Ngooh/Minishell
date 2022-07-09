0363ace5e50b526e37483cf3576e48c547dc275c
Erick-Ngooh <erickngooh@gmail.com>
2022-07-09 03:26:33 +0200

    doc terminale de commande 2

---
+++ cod/Minishell.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: engooh <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/08 19:57:17 by engooh            #+#    #+#             */
+/*   Updated: 2022/07/09 03:11:01 by engooh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+#define MINISHEL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <readline/readline.h>
+#include <readline/history.h> 
+#include "./share/libft/libft.h"
+
+
+
+#endif
